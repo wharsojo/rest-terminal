6c0e6380338f7704be4e15b7a47412543d58cd4d
widi harsojo <wharsojo@Gomita.local>
2012-10-29 16:00:11 +0700

    add bin command "rt"

--- .rest-terminal/persistent_rc.rb
+++ .rest-terminal/persistent_rc.rb
@@ -21,7 +21,7 @@ module Rest
         #env_start
         @pwd  = "/stripe/"
         @serv = "localhost"
-        @hist = [:ls, :pwd, :init, "pwd", "cd", "ls", "body", "info", "send"]
+        @hist = [:ls, :pwd, :init, "pwd", "cd", "ls", "body", "send", "info"]
         @response = {:status=>200, :headers=>{"server"=>"nginx", "date"=>"Mon, 29 Oct 2012 08:53:35 GMT", "content-type"=>"application/json;charset=utf-8", "content-length"=>"600", "connection"=>"close", "access-control-allow-credentials"=>"true", "cache-control"=>"no-cache, no-store", "access-control-max-age"=>"300"}, :body=>{"amount"=>0, "created"=>1351500815, "currency"=>"usd", "id"=>"tok_0dVJaOA5AOt8Yk", "livemode"=>false, "object"=>"token", "used"=>false, "card"=>{"address_city"=>nil, "address_country"=>nil, "address_line1"=>nil, "address_line1_check"=>nil, "address_line2"=>nil, "address_state"=>nil, "address_zip"=>nil, "address_zip_check"=>nil, "country"=>"US", "cvc_check"=>nil, "exp_month"=>5, "exp_year"=>2013, "fingerprint"=>"PvKkd17TxyqPEYGt", "last4"=>"4242", "name"=>nil, "object"=>"card", "type"=>"Visa"}}}
         #env_end
       end
--- rest-terminal/rest-terminal.gemspec
+++ rest-terminal/rest-terminal.gemspec
@@ -11,12 +11,12 @@ Gem::Specification.new do |s|
 
 	s.rubyforge_project = "rest-terminal"
 
-	s.files      = %w[bin/rest lib/rest.rb lib/service_base.rb lib/rest/terminal.rb lib/rest/terminal/commands.rb lib/rest/terminal/commands_info.rb lib/rest/terminal/persistent.rb lib/rest/terminal/persistent_rc.rb lib/rest/service.rb lib/rest/version.rb] 
+	s.files      = %w[bin/rest bin/rt lib/rest.rb lib/service_base.rb lib/rest/terminal.rb lib/rest/terminal/commands.rb lib/rest/terminal/commands_info.rb lib/rest/terminal/persistent.rb lib/rest/terminal/persistent_rc.rb lib/rest/service.rb lib/rest/version.rb] 
 
 	s.test_files = []
 
 	s.require_paths = ["lib"] 
-    s.executables   = ["rest"]
+    s.executables   = ["rest","rt"]
 	s.add_runtime_dependency "term-ansicolor"
 	s.add_runtime_dependency "faraday"
 	s.add_runtime_dependency "rake"
